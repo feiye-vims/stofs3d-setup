8b625bd07003b9c128d7788637d23a67a19657b8
Fei Ye <feiye@vims.edu>
2026-03-03 03:26:35 -0500

    Added a script for sigma vgrid. Other minor changes.

--- make_inputs.py
+++ make_inputs.py
@@ -2,7 +2,7 @@
 from stofs3d_setup.config.schema import Settings
 from stofs3d_setup.recipes.generic import build
 
-cfg = Settings.from_yaml("/sciclone/data10/feiye/stofs3d-setup/configs/la_v8_2023_hindcast_R33.yml")
+cfg = Settings.from_yaml("/sciclone/data10/feiye/stofs3d-setup/configs/la_v8_2023_hindcast_R36.yml")
 build(cfg)
 
 pass
